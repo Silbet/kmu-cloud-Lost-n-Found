3da756092e1386996b1565c0f925ae4c39d69b27
Silbet <gs1237@kookmin.ac.kr>
2026-05-25 19:55:03 +0900

    feat: 습득물 검색 API 구현

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { MatchingModule } from './matching/matching.module';
+import { ItemsModule } from './items/items.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ReportsModule } from './reports/reports.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
   imports: [
@@ -15,6 +17,8 @@ import { ReportsModule } from './reports/reports.module';
     MatchingModule,
     AuthModule,
     ReportsModule,
+    ItemsModule,
+    SearchModule,
   ],
   controllers: [AppController],
 })
